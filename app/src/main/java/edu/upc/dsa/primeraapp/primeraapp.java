e6d796329c960f59b1b21484ea041fc8eb85b838
vcarl <vcarlos139@gmail.com>
2020-03-18 21:39:48 +0100

    initial commit

---
+++ app/src/main/java/edu/upc/dsa/primeraapp/primeraapp.java
@@ -0,0 +1,14 @@
+package edu.upc.dsa.primeraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class primeraapp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeraapp);
+    }
+}
